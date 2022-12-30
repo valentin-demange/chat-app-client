a144eb4737d8ca183ad88f15bb0906dcb497b0c9
valentin-demange <demange.valentin@gmail.com>
2022-12-30 16:10:26 +0100

    Remove firebase from avatarUser

--- components/Others/avatarUser.tsx
+++ components/Others/avatarUser.tsx
@@ -1,20 +1,22 @@
 import { Avatar } from "@chakra-ui/react";
 import React from "react";
-import { db } from "utils/firebase";
-import { doc } from "firebase/firestore";
-import { useDocumentData } from "react-firebase-hooks/firestore";
 
 export default function AvatarUser({ uid } : {uid:string}) {
-  const [userInfo, loading, error] = useDocumentData(doc(db, "users", uid), {
-    snapshotListenOptions: { includeMetadataChanges: true },
-  });
+  // const [userInfo, loading, error] = useDocumentData(doc(db, "users", uid), {
+  //   snapshotListenOptions: { includeMetadataChanges: true },
+  // });
 
-  if (loading) return <></>;
-  if (error) return <div>Error</div>;
+  const userInfo = {
+    name: "Valentin",
+    photoURL: "",
+  }
+
   if (userInfo) {
     return <div>
       <Avatar name={userInfo.name} src={userInfo.photoURL} backgroundColor="gray.100"/>
     </div>;
   }
+  // if (loading) return <></>;
+  // if (error) return <div>Error</div>;
   return <></>;
 }
