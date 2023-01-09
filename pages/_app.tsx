0c6b9998c20d120a1b5042553a5e03b63a0811bb
valentin-demange <demange.valentin@gmail.com>
2023-01-09 10:21:36 +0100

    Add support for websockets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,18 @@ import "@/styles/global.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { extendTheme, withDefaultColorScheme } from '@chakra-ui/react'
+import { io } from "socket.io-client";
+
+const socket = io('http://localhost:3000', { transports : ['websocket'] });
+
+socket.on('connect', () => {
+  console.log('Connected to WebSockets server');
+  socket.send('Hello from the client!');
+});
+
+socket.on('message', (message: any) => {
+  console.log(`Received message: ${message}`);
+});
 
 const customTheme = extendTheme(withDefaultColorScheme({ colorScheme: 'gray' }))
 
