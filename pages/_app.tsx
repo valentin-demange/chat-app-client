3ecd75a7be6ee45ec6c89df02a7dd4be4c911152
valentin-demange <demange.valentin@gmail.com>
2023-01-12 10:17:59 +0100

    Clean file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import "@/styles/global.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { extendTheme, withDefaultColorScheme } from "@chakra-ui/react";
-import { io } from "socket.io-client";
-import { SocketContext } from "utils/context";
 
 const customTheme = extendTheme(
   withDefaultColorScheme({ colorScheme: "gray" })
