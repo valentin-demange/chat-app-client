80770811cca92fccc54dd68e9c964908ed557819
valentin-demange <demange.valentin@gmail.com>
2023-01-14 12:06:07 +0100

    Fix error

--- config.ts
+++ config.ts
@@ -1,4 +1,2 @@
-import 'dotenv/config'
-
 export const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000"
 export const NEXT_PUBLIC_ENV = process.env.NEXT_PUBLIC_ENV || 'dev'
