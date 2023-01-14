a2ff26dca7b17264b496bc26407b98153ca4ced8
valentin-demange <demange.valentin@gmail.com>
2023-01-14 11:24:29 +0100

    Add config file

---
+++ config.ts
@@ -0,0 +1,7 @@
+import 'dotenv/config'
+
+// export const PORT = process.env.PORT || 3000
+// export const NODE_ENV = process.env.NODE_ENV || 'dev'
+// export const DATABASE_URL = process.env.DATABASE_URL || "postgre"
+// export const SESSION_SECRET = process.env.SESSION_SECRET || 'anything'
+// export const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3001'
