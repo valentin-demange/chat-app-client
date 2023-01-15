96861aad30f759bf7ba4b9a8792566fe89ee87b8
valentin-demange <demange.valentin@gmail.com>
2023-01-15 09:38:34 +0100

    Add a test button on home page

--- components/Login/homeMenu.tsx
+++ components/Login/homeMenu.tsx
@@ -1,8 +1,33 @@
 import React from "react";
 import styles from "./styles.module.css";
 import { Box, Button } from "@chakra-ui/react";
+import { API_URL } from "config";
 
 export default function({cb} : {cb:any}) {
+
+  const onClick = async (values: any, actions: any) => {
+    // event.preventDefault();
+    try {
+      const res = await fetch(`${API_URL}/api/users/current`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+        // body: JSON.stringify(values),
+        credentials: 'include',
+            });
+      if (!res.ok) {
+        const message = await res.text();
+        throw new Error([res.statusText, message].join("\n"));
+      }
+      const result = await res.json();
+      console.log(result)
+      // Navigate to the /chats route using the Router object
+    } catch (error: any) {
+      alert(error.message);
+    } finally {
+    }
+  };
+
+  
   return (
     <div className={styles.homeButtonContainer}>
       <div>
@@ -13,6 +38,11 @@ export default function({cb} : {cb:any}) {
       <div>
         <Button className={styles.homeButton} colorScheme="blue" variant="outline" onClick={cb.goToLogin}>
           Log in
+        </Button>
+      </div>
+      <div>
+        <Button className={styles.homeButton} colorScheme="green" onClick={onClick as any}>
+          Test
         </Button>
       </div>
     </div>
